c690d2357d635ce21d47d1333f2c01f79ec7a343
ArtyrRoze <a.r.c.h.y@rambler.ru>
2019-01-25 18:42:39 +0200

    convertDateStringToTimestamp done

--- src/test/java/com/app/utils/DateConverterUtilTest.java
+++ src/test/java/com/app/utils/DateConverterUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,9 +20,11 @@ public class DateConverterUtilTest {
         String stringDate = "2018-09-19T13:12:21.136263+03:00";
 
         //action
+        Instant instant = DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(stringDate, Instant::from);
         Timestamp timestamp = DateConverterUtil.convertStringDateToTimestamp(stringDate);
 
         //assert
-        assertEquals(timestamp.toString(), stringDate);
+        assertEquals("2018-09-19T10:12:21.136263Z", instant.toString());
+        assertEquals(instant, timestamp.toInstant());
     }
 }
